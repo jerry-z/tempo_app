93a1d1e6bf154fb8b8a18811d497f719029591b5
Jerry Zhang <jz2966@columbia.edu>
2019-12-08 20:08:58 -0500

    frontend files

---
+++ main.js
@@ -0,0 +1,45 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+function goto_home(){location.href = "../home/home.html";}
+function goto_search(){location.href = "../search/search.html";}
+function goto_upload(){location.href = "../upload/upload.html";}
+function goto_user(){location.href = "../user/user.html";}
+
+
+function likebutton(x) {
+    if ( x.classList.contains( "fa-heart") ) {
+        x.classList.remove( "fa-heart" );
+        x.classList.add( "fa-heart-o" );
+    }
+    else {
+        x.classList.remove( "fa-heart-o" );
+        x.classList.add( "fa-heart" );
+    }
+}
+
+
+
+function savebutton(x) {
+  if ( x.classList.contains( "far") ) {
+        x.classList.remove( "far" );
+        x.classList.add( "fas" );
+    }
+    else {
+        x.classList.remove( "fas" );
+        x.classList.add( "far" );
+    }
+
+}
+
